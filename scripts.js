f0f286ba55376de44666f09c9d378ec3275153a3
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-07-30 13:00:58 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 async function loginUser(email, password) {
-    const response = await fetch('https://your-api-url/login', {
+    const response = await fetch('https://url', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
